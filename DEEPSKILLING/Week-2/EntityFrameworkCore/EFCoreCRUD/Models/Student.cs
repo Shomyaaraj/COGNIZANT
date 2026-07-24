c3cf6f598ef279657ac58f28d8bc337883180ba6
Shomyaaraj <shomya725@gmail.com>
2026-07-24 10:02:04 +0530

    all structure redefined

--- DEEPSKILLING/Week-2/EntityFrameworkCore/EFCoreCRUD/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-namespace EFCoreCRUD.Models
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public int Age { get; set; }
-    }
-}
